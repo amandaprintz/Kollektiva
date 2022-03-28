ef473bc8d2d04f2adb697d90fbf170c7a4ef0bc4
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2022-03-28 19:00:12 +0200

    Breakpoints added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,31 +1,11 @@
 @include('header')
-
-@if ($errors->any())
-<p>
-    <u>{{ $errors->first() }}</u>
-</p>
-@endif
-<main>
-    <h1>Hi!</h1>
-    <p>Already a member? Log in here!</p>
-    <div class="signUp">
-        <form method="post" action="/login">
-            @csrf
-            <div>
-                <label for="email">Email</label><br>
-                <input name="email" id="email" type="email" />
-            </div>
-            <br>
-            <br>
-            <div>
-                <label for="password"> Password</label><br>
-                <input name="password" id="password" type="password" />
-            </div>
-
-            <button type="submit">Login</button>
-        </form>
-        <a href="{{ route('register') }}">Create an account</a>
-    </div>
-</main>
-
+    <section class="hero">
+        <div class="hero-content">
+            <h1>Välkommen till Kollektiva!</h1>
+            <p>Skapa ett konto idag, snabbt och enkelt.</p>
+            <p>Här kan ni välja mellan att annonsera ert utrymme eller bara bläddra bland tillgängliga lokaler.</p>
+            <a href="#">Läs mer om Kollektiva</a>
+        </div>
+        <img src="https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3000&q=80" alt="hero image">
+    </section>
 @include('footer')
---
+++ resources/views/login.blade.php
@@ -0,0 +1,31 @@
+@include('header')
+
+@if ($errors->any())
+<p>
+    <u>{{ $errors->first() }}</u>
+</p>
+@endif
+<main>
+    <h1>Hi!</h1>
+    <p>Already a member? Log in here!</p>
+    <div class="signUp">
+        <form method="post" action="/login">
+            @csrf
+            <div>
+                <label for="email">Email</label><br>
+                <input name="email" id="email" type="email" />
+            </div>
+            <br>
+            <br>
+            <div>
+                <label for="password"> Password</label><br>
+                <input name="password" id="password" type="password" />
+            </div>
+
+            <button type="submit">Login</button>
+        </form>
+        <a href="{{ route('register') }}">Create an account</a>
+    </div>
+</main>
+
+@include('footer')
