3d50f38b8cc4dfd89b22f72a714fefbd0e203a67
amandaprintz <fannyamandakarlsson@gmail.com>
2022-03-29 15:24:33 +0200

    Sass boxes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,13 +7,26 @@
             <p>Här kan ni välja mellan att annonsera ert utrymme eller bara bläddra bland tillgängliga lokaler.</p>
             <a href="#">Läs mer om Kollektiva</a>
         </div>
-
         <div class="hero-buttons">
             <button>Skapa gratiskonto</button>
             <button>Logga in</button>
         </div>
     </div>
-
     <img src="https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3000&q=80" alt="hero image">
 </section>
-@include('footer')
+
+<section class="two-card">
+    <div class="option">
+        <h2>För hyresvärd</h2>
+
+        <button>Skapa annonser</button>
+    </div>
+    <div class="option">
+        <h2>För Bostadssökande</h2>
+
+        <button>Bläddra bland annonser</button>
+    </div>
+
+
+
+</section> @include('footer')
