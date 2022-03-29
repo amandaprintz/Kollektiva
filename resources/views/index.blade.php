5c3ec6d66c994d3d2f266fa0b43b2489c1ca398a
amandaprintz <fannyamandakarlsson@gmail.com>
2022-03-29 13:48:00 +0200

    Added hero-buttons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,19 @@
 @include('header')
-    <section class="hero">
-        <div class="hero-content">
+<section class="hero">
+    <div class="hero-content">
+        <div class="hero-text">
             <h1>Välkommen till Kollektiva!</h1>
             <p>Skapa ett konto idag, snabbt och enkelt.</p>
             <p>Här kan ni välja mellan att annonsera ert utrymme eller bara bläddra bland tillgängliga lokaler.</p>
             <a href="#">Läs mer om Kollektiva</a>
         </div>
-        <img src="https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3000&q=80" alt="hero image">
-    </section>
+
+        <div class="hero-buttons">
+            <button>Skapa gratiskonto</button>
+            <button>Logga in</button>
+        </div>
+    </div>
+
+    <img src="https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3000&q=80" alt="hero image">
+</section>
 @include('footer')
