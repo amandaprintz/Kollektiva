ca3d5e48cb26991a695c7eddfadf24f72cba912d
amandaprintz <fannyamandakarlsson@gmail.com>
2022-03-31 14:31:15 +0200

    Started footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,40 @@
     <footer>
-        This is a footer!
+        <section class="footer-card">
+            <div class="footer-container">
+                <div class="footer-start">
+                    <h2>Kollektiva</h2>
+                    <p>Du kan enkelt skapa ett konto med bara mail och lösenord, det är självklart gratis!</p>
+                </div>
+                <div class="vl"></div>
+
+                <div class="footer-option">
+                    <h4>OM OSS</h4>
+                    <a class="footer-items" href="#">Grundarna</a>
+                    <a class="footer-items" href="#">Varför Kollektiva</a>
+                    <a class="footer-items" href="#">Om medlemskapet</a>
+                    </p>
+                </div>
+                <div class="vl"></div>
+
+                <div class="footer-option">
+                    <h4>SÖK</h4>
+                    <a class="footer-items" href="#">Bostad</a> <br>
+                    <a class="footer-items" href="#">Boendesökande</a>
+                    <a class="footer-items" href="#">Vanliga frågor</a>
+                </div>
+                <div class="vl"></div>
+
+                <div class="footer-option">
+                    <h4>KONTAKT</h4>
+                    <a class="footer-items" href="#">Gregler</a>
+                    <a class="footer-items" href="#">Allmäna villkor</a>
+                    <a class="footer-items" href="#">Kontakt</a>
+                </div>
+            </div>
+        </section>
+
+
     </footer>
-</body>
-</html>
+    </body>
+
+    </html>
