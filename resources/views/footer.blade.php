ecae9258bd34d4feadf32d3342ab1b1304ae4e2f
amandaprintz <fannyamandakarlsson@gmail.com>
2022-03-31 14:50:55 +0200

    Continue footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,32 +1,35 @@
     <footer>
-        <section class="footer-card">
+        <section class="footer">
             <div class="footer-container">
                 <div class="footer-start">
                     <h2>Kollektiva</h2>
-                    <p>Du kan enkelt skapa ett konto med bara mail och lösenord, det är självklart gratis!</p>
+                    <p>Hos Kollektiva kan du både
+                        hyra ut och söka bostäder.
+                        Med Kollektivas matchnings-
+                        system blir det enkelt att
+                        hitta rätt person för dig. </p>
+                    <div class="vl"></div>
                 </div>
-                <div class="vl"></div>
-
                 <div class="footer-option">
                     <h4>OM OSS</h4>
                     <a class="footer-items" href="#">Grundarna</a>
                     <a class="footer-items" href="#">Varför Kollektiva</a>
                     <a class="footer-items" href="#">Om medlemskapet</a>
                     </p>
-                </div>
-                <div class="vl"></div>
 
+                    <div class="vl"></div>
+                </div>
                 <div class="footer-option">
                     <h4>SÖK</h4>
                     <a class="footer-items" href="#">Bostad</a> <br>
                     <a class="footer-items" href="#">Boendesökande</a>
                     <a class="footer-items" href="#">Vanliga frågor</a>
-                </div>
-                <div class="vl"></div>
 
+                    <div class="vl"></div>
+                </div>
                 <div class="footer-option">
                     <h4>KONTAKT</h4>
-                    <a class="footer-items" href="#">Gregler</a>
+                    <a class="footer-items" href="#">Regler</a>
                     <a class="footer-items" href="#">Allmäna villkor</a>
                     <a class="footer-items" href="#">Kontakt</a>
                 </div>
