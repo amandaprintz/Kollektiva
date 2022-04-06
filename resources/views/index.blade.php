e0d503a373b16096494e0b19deec618b51a3c984
amandaprintz <fannyamandakarlsson@gmail.com>
2022-04-06 20:44:47 +0200

    Added number cards on options

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,13 +35,6 @@
     <h2>Hur funkar det?</h2>
     <div class="option-container">
         <div class="option yellow">
-<<<<<<< Updated upstream
-            <h3>1. Skapa konto</h3>
-            <p>Du kan enkelt skapa ett konto med bara mail och lösenord, det är självklart gratis!</p>
-        </div>
-        <div class="option mint">
-            <h3>2. Annonsera bostad</h3>
-=======
             <div class="option-top">
                 <h2>1.</h2>
                 <h3>Skapa konto</h2>
@@ -53,18 +46,16 @@
                 <h2>2.</h2>
                 <h3>Annonsera bostad</h3>
             </div>
->>>>>>> Stashed changes
             <p>Du kan enkelt skapa ett konto med bara mail och lösenord, det är självklart gratis!</p>
 
         </div>
         <div class="option red">
-<<<<<<< Updated upstream
-            <h3>3. Bläddra bland ansökningar</h3>
-            <p>Du kan enkelt skapa ett konto med bara mail och lösenord, det är självklart gratis!</p>
+            <<<<<<< Updated upstream <h3>3. Bläddra bland ansökningar</h3>
+                <p>Du kan enkelt skapa ett konto med bara mail och lösenord, det är självklart gratis!</p>
         </div>
         <div class="option purple">
             <h3>4. Kontakta genom Kollektiva</h3>
-=======
+            =======
             <div class="option-top">
                 <h2>3.</h2>
                 <h3>Bläddra bland ansökningar</h2>
@@ -76,7 +67,7 @@
                 <h2>4.</h2>
                 <h3>Kontakta genom Kollektiva</h2>
             </div>
->>>>>>> Stashed changes
+            >>>>>>> Stashed changes
             <p>Du kan enkelt skapa ett konto med bara mail och lösenord, det är självklart gratis!</p>
         </div>
     </div>
