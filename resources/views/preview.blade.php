0c67a034e79caec926d6ef4fd9d68b96e746fd95
amandaprintz <fannyamandakarlsson@gmail.com>
2022-04-07 16:47:09 +0200

    Started content ad page

--- resources/views/preview.blade.php
+++ resources/views/preview.blade.php
@@ -1,1 +1,11 @@
 @include('header')
+<main>
+
+    <section class="preview-intro">
+        <h2>Förhandsgranskning annons</h2>
+    </section>
+
+    <section class="ad-card">
+    </section>
+
+</main>
