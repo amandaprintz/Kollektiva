a9f6813e6ca928265355a865f1f31d1d9f2a73cb
amandaprintz <fannyamandakarlsson@gmail.com>
2022-04-08 14:31:51 +0200

    Preview ad

--- resources/views/preview.blade.php
+++ resources/views/preview.blade.php
@@ -5,7 +5,35 @@
         <h2>Förhandsgranskning annons</h2>
     </section>
 
-    <section class="ad-card">
+
+
+    <section class="preview-ad-card">
+        <div class="preview-container">
+            <div class="preview-image">
+                <img src="https://images.unsplash.com/photo-1593604341107-12f3981d929b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" alt="update image">
+            </div>
+            <div class="preview-text">
+                <h2>Trädgårdsgatan 43</h2>
+                <p>Centrala Hisingen, Göteborg</p>
+                <hr>
+
+
+                <br>
+
+                <p>Välkommen till denna pärla perfekt belägen på centrala Hisingen.
+                    Tack vare bostadens läge i huset ges ett ostört läge så gott som fritt från insyn.
+                    Bostaden präglas av fint ljusinsläpp från stora fönsterpartier med utsikt mot vacker
+                    grönska.</p>
+
+                <div class="tag-container">
+                    <button class="tags">Hus</button>
+                    <button class="tags">Hiss</button>
+                    <button class="tags">Fiber</button>
+                    <button class="tags">Balkong</button>
+                </div>
+            </div>
+        </div>
     </section>
 
 </main>
+@include('footer')
