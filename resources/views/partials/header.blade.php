2f6fba26908a9b1ec2f32b4b771a0ed061e2f7a1
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2022-04-21 11:04:58 +0200

    Minor adjustments for preview page

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,10 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/svg" href="../../../public/images/logo-mobile.svg">
     <title>Kollektiva</title>
     <!-- Styles -->
     <link rel="stylesheet" href="/css/app.css">
     <script src="{{ asset('js/app.js') }}"></script>
+
 
 </head>
 
